e9027fef87443464e5644975b077eb011151c54b
skindervik <38836685+skindervik@users.noreply.github.com>
2021-10-30 13:07:15 +0300

    webpack dev server

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,17 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
 module.exports = {
   mode: "development",
   module: {
     rules: [
       {
+        test: /\.js$/,
+        include: /src/,
         use: {
           loader: "swc-loader",
         },
       },
     ],
   },
+  plugins: [new HtmlWebpackPlugin({ template: "./public/index.html" })],
 };
